55704d12e52cf2f13f1208d0372fa6022f29df0b
Heng Zheng Kai <zkheng.2023@scis.smu.edu.sg>
2024-10-14 23:07:38 +0800

    commit msg

---
+++ script.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+        return {
+          // name:value pairs
+        }
+    },
+    mounted(){
+        console.log("Hello World")
+
+    },
+
+    methods: {
+    }
+})
+
+app.mount('#app')
