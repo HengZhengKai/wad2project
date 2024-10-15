f0199e69f0393ea041c6669e56a8daf51ef2a929
Heng Zheng Kai <zkheng.2023@scis.smu.edu.sg>
2024-10-15 16:16:02 +0800

    created new files

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// index.html
 const home = Vue.createApp({
 
     data() {
@@ -13,3 +14,54 @@ const home = Vue.createApp({
 })
 
 home.mount('#home')
+
+// products.html
+const products = Vue.createApp({
+
+    data() {
+        return {
+        }
+    },
+
+    mounted(){
+    },
+
+    methods: {
+    }
+})
+
+products.mount('#products')
+
+// locations.html
+const locations = Vue.createApp({
+
+    data() {
+        return {
+        }
+    },
+
+    mounted(){
+    },
+
+    methods: {
+    }
+})
+
+locations.mount('#locations')
+
+// about.html
+const about = Vue.createApp({
+
+    data() {
+        return {
+        }
+    },
+
+    mounted(){
+    },
+
+    methods: {
+    }
+})
+
+about.mount('#about')
